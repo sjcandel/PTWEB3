4744daaf2dc2573ca011cc538c33b334a97a505d
Talinona1 <timothe.golla@etu.unistra.fr>
2016-10-24 19:03:54 +0200

    page "qui sommes-nous" terminées

--- pages/presentation.php
+++ pages/presentation.php
@@ -1,8 +1,16 @@
 <!DOCTYPE html>
 <html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Vue du Coeur</title>
+    <link rel="stylesheet" href="../styles/reset.css">
+    <link rel="stylesheet" href="../styles/font-awesome-4.6.3/css/font-awesome.css">
+    <link rel="stylesheet" href="../styles/animate.css">
+    <link rel="stylesheet" href="../styles/timothe.css">
+</head>
     <!--HEADER-->
     <?php include("../includes/header.php"); ?>
-    
+
     <body>
         <!--NAV-->
         <?php include("../includes/nav.php"); ?>
@@ -12,9 +20,83 @@
         <main>
            <div class="wrapper-bleu">
 
-                <!-- insérez votre contenu ici -->
+              <div class="presentation_haut">
+                <h2 class="titre_principal texte_fonce">Qui sommes-nous ?</h2>
+                <br /><br />
+                <p class="texte_fonce">« Vue du cœur » a pour vocation de favoriser
+                  le développement et l’épanouissement d’enfants déficients
+                  visuels et non-voyants en pratiquant le «vivre ensemble» entre
+                  enfants voyants et non-voyants, en développant leur autonomie
+                  et leur mobilité et en encourageant leur confiance en eux et
+                  à s’ouvrir au monde.<br>
+                  L'association a recours aux nouvelles technologies, organise des
+                  sorties et activités adaptées à l’âge et aux besoins des
+                  enfants et, surtout, s'appuie sur l’expérience de jeunes adultes
+                  déficients visuellement et d’autres associations de proximité.</p>
+                  <div class="image_presentation"></div>
+              </div>
 
-           </div> 
+              <div class="presentation_bas">
+                <div class="presentation_droite">
+                  <h3 class="titre_secondaire texte_fonce">Historique</h3>
+                  <br />
+                  <p class="texte_fonce">Nathan est né en 2007 en créant la surprise :
+                    il ne verrait jamais car ses nerfs optiques étaient trop fins
+                    pour laisser passer une quelconque information à son cerveau.<br />
+                    Mais bien vite ses parents se sont rendu compte qu’il pouvait
+                    voir avec son cœur !<br />
+                    Convaincus, amis et proches de la famille se sont dit que
+                    quelques paires de bras supplémentaires ne seraient pas
+                    superflues pour lui permettre de développer son don et expliquer
+                    aux autres de quoi il retourne.</p>
+                  <br />
+                  <p class="texte_fonce">Et c’est ainsi que nous avons décidé de
+                    créer l’association Vue du cœur : à plusieurs, ça devient
+                    une aventure, là où seuls cela peut être la galère.<br />
+                    Depuis avril 2012, Nathan nous guide et à travers lui nous
+                    découvrons ensemble le monde de la non-voyance.<br />
+                    Nous essayons de sensibiliser à cette différence et de partager
+                    nos expériences.<br />
+                    Bien sur, l’association cherche des fonds afin de financer
+                    les onéreux matériels et livres adaptés.<br />
+                    Mais aucune somme d’argent ne remplacera l’ingéniosité, les
+                    trucs et astuces, et les conseils de ceux qui œuvrent de leur
+                    main pour pallier le manque de ce qui pour les enfants
+                    ordinaires est tout simplement normal.</p>
+                </div>
+
+                <div class="cadre_jaune">
+                  <div class="texte_cadre_jaune">
+                    <h3 class="texte_jaune titre_secondaire">Nos membres</h3>
+                    <br />
+                    <p class="texte_clair">
+                      <span>Nom Prenom</span> - Fonction<br />
+                      06.00.00.00.00<br />
+                      adresse@mail.com
+                    </p>
+                    <br />
+                    <p class="texte_clair">
+                      <span>Nom Prenom</span> - Fonction<br />
+                      06.00.00.00.00<br />
+                      adresse@mail.com
+                    </p>
+                    <br />
+                    <p class="texte_clair">
+                      <span>Nom Prenom</span> - Fonction<br />
+                      06.00.00.00.00<br />
+                      adresse@mail.com
+                    </p>
+                    <br />
+                    <p class="texte_clair">
+                      <span>Nom Prenom</span> - Fonction<br />
+                      06.00.00.00.00<br />
+                      adresse@mail.com
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+           </div>
         </main>
 
 
