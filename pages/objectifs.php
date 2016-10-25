56ec474de6017492140e8a87760c1c36f319d506
Talinona1 <timothe.golla@etu.unistra.fr>
2016-10-25 12:20:27 +0200

    page objectifs terminée

--- pages/objectifs.php
+++ pages/objectifs.php
@@ -1,8 +1,16 @@
 <!DOCTYPE html>
 <html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Vue du Coeur</title>
+    <link rel="stylesheet" href="../styles/reset.css">
+    <link rel="stylesheet" href="../styles/font-awesome-4.6.3/css/font-awesome.css">
+    <link rel="stylesheet" href="../styles/animate.css">
+    <link rel="stylesheet" href="../styles/timothe.css">
+</head>
     <!--HEADER-->
     <?php include("../includes/header.php"); ?>
-    
+
     <body>
         <!--NAV-->
         <?php include("../includes/nav.php"); ?>
@@ -12,9 +20,56 @@
         <main>
            <div class="wrapper-bleu">
 
-                <!-- insérez votre contenu ici -->
+              <div class="presentation_haut">
+                <h2 class="titre_principal texte_fonce">Nos activités et objectifs</h2>
+                <br /><br />
+                <p class="texte_fonce">Nous voulons initier les autres et nous
+                  même à aider ces enfants malvoyants et non-voyants afin qu’ils
+                  puissent apprendre et s’amuser grâce à des sorties et des jeux,
+                  le tout avec des personnes voyantes et non-voyantes pour leur
+                  donner une certaine autonomie et apprendre le “vivre ensemble”
+                  à tout le monde.</p>
+              </div>
 
-           </div> 
+              <div class="presentation_bas">
+                <div class="presentation_droite">
+                  <div class="objectif_image_haut"></div>
+                  <div class="objectif_image_bas"></div>
+                </div>
+
+                <div class="cadre_jaune cadre_jaune_objectif">
+                  <div class="texte_cadre_jaune">
+                    <h3 class="texte_jaune titre_secondaire">Comment ?</h3>
+                    <br />
+                    <p class="texte_fonce">
+                      <span>1- </span>Par l’utilisation de technologies modernes
+                      de navigation. Les enfants malvoyants et non-voyants peuvent
+                      découvrir des environnements naturels ou urbains grâce à
+                      des outils GPS de navigation spécifiques.
+                    </p>
+                    <br />
+                    <p class="texte_fonce"><p class="texte_fonce">
+                      Nous faisons cette activité avec des adultes déficients
+                      visuels qui ont l’expérience de ces outils et qui veulent
+                      la partager.
+                    </p>
+                    <br /><br />
+                    <p class="texte_fonce">
+                      <span>2- </span>Par l’organisation de sorties communes de
+                      natures diverses (ludiques / culturelles / sportives) et
+                      par d’autres moyens adaptés à l’âge et à la situation des
+                      enfants.
+                    </p>
+                    <br />
+                    <p class="texte_fonce">
+                      Nous organisons, à l’occasion, des partenariats avec
+                      d’autres associations ou des écoles.
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+           </div>
         </main>
 
 
