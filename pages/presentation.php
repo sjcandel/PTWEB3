8981737800f8012f0593cdc7ff3b9389973c1e93
Talinona1 <timothe.golla@etu.unistra.fr>
2016-10-25 22:48:19 +0200

    Correction légère html et hiérarchie css

--- pages/presentation.php
+++ pages/presentation.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="../styles/reset.css">
     <link rel="stylesheet" href="../styles/font-awesome-4.6.3/css/font-awesome.css">
     <link rel="stylesheet" href="../styles/animate.css">
+    <link rel="stylesheet" href="../styles/style.css">
     <link rel="stylesheet" href="../styles/timothe.css">
 </head>
     <!--HEADER-->
