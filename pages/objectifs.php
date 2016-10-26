14e7a746c41ce924c507b4eb54e7228a603e611a
Talinona1 <timothe.golla@etu.unistra.fr>
2016-10-26 15:00:01 +0200

    enlevage des br

--- pages/objectifs.php
+++ pages/objectifs.php
@@ -5,18 +5,23 @@
 <?php include("../includes/header.php"); ?>
 
     <body>
-       
+
         <!--NAV-->
         <?php include("../includes/nav.php"); ?>
-           
+
         <!--MAIN-->
             <main>
                 <div class="wrapper-bleu">
                     <div class="presentation_haut">
                         <h2 class="titre_principal texte_fonce">Nos activités et objectifs</h2>
-                        <br />
-                        <br />
-                        <p class="texte_fonce">Nous voulons initier les autres et nous même à aider ces enfants malvoyants et non-voyants afin qu’ils puissent apprendre et s’amuser grâce à des sorties et des jeux, le tout avec des personnes voyantes et non-voyantes pour leur donner une certaine autonomie et apprendre le “vivre ensemble” à tout le monde.</p>
+
+                        <p class="texte_fonce">Nous voulons initier les autres et
+                          nous même à aider ces enfants malvoyants et non-voyants
+                          afin qu’ils puissent apprendre et s’amuser grâce à des
+                          sorties et des jeux, le tout avec des personnes voyantes
+                          et non-voyantes pour leur donner une certaine autonomie
+                          et apprendre le “vivre ensemble” à tout le monde.
+                        </p>
                     </div>
                     <div class="presentation_bas">
                         <div class="presentation_droite">
@@ -26,22 +31,37 @@
                         <div class="cadre_jaune cadre_jaune_objectif">
                             <div class="texte_cadre_jaune">
                                 <h3 class="texte_jaune titre_secondaire">Comment ?</h3>
-                                <br />
-                                <p class="texte_fonce"> <span>1- </span>Par l’utilisation de technologies modernes de navigation. Les enfants malvoyants et non-voyants peuvent découvrir des environnements naturels ou urbains grâce à des outils GPS de navigation spécifiques. </p>
-                                <br />
-                                <p class="texte_fonce">
-                                    <p class="texte_fonce"> Nous faisons cette activité avec des adultes déficients visuels qui ont l’expérience de ces outils et qui veulent la partager. </p>
-                                    <br />
-                                    <br />
-                                    <p class="texte_fonce"> <span>2- </span>Par l’organisation de sorties communes de natures diverses (ludiques / culturelles / sportives) et par d’autres moyens adaptés à l’âge et à la situation des enfants. </p>
-                                    <br />
-                                    <p class="texte_fonce"> Nous organisons, à l’occasion, des partenariats avec d’autres associations ou des écoles. </p>
+
+                                <p class="texte_fonce"> <span>1- </span>Par
+                                  l’utilisation de technologies modernes de navigation.
+                                  Les enfants malvoyants et non-voyants peuvent
+                                  découvrir des environnements naturels ou urbains
+                                  grâce à des outils GPS de navigation spécifiques.
+                                </p>
+
+                                <p class="texte_fonce"> Nous faisons cette
+                                  activité avec des adultes déficients visuels
+                                  qui ont l’expérience de ces outils et qui
+                                  veulent la partager.
+                                </p>
+
+                                <p class="texte_fonce"> <span>2- </span>Par
+                                  l’organisation de sorties communes de
+                                  natures diverses (ludiques / culturelles
+                                  / sportives) et par d’autres moyens adaptés
+                                  à l’âge et à la situation des enfants.
+                                </p>
+
+                                <p class="texte_fonce"> Nous organisons, à l’occasion,
+                                  des partenariats avec d’autres associations ou
+                                  des écoles.
+                                </p>
                             </div>
                         </div>
                     </div>
                 </div>
             </main>
-            
+
             <!--FOOTER-->
             <?php include("../includes/footer.php"); ?>
     </body>
--- pages/presentation.php
+++ pages/presentation.php
@@ -8,50 +8,81 @@
     <body>
         <!--NAV-->
         <?php include("../includes/nav.php"); ?>
-        
+
         <!--MAIN-->
         <main>
             <div class="wrapper-bleu">
                 <div class="presentation_haut">
                     <h2 class="titre_principal texte_fonce">Qui sommes-nous ?</h2>
-                    <br />
-                    <br />
-                        <p class="texte_fonce">« Vue du cœur » a pour vocation de favoriser le développement et l’épanouissement d’enfants déficients visuels et non-voyants en pratiquant le «vivre ensemble» entre enfants voyants et non-voyants, en développant leur autonomie et leur mobilité et en encourageant leur confiance en eux et à s’ouvrir au monde.</p>
+
+                        <p class="texte_fonce">« Vue du cœur » a pour vocation
+                          de favoriser le développement et l’épanouissement
+                          d’enfants déficients visuels et non-voyants en pratiquant
+                          le «vivre ensemble» entre enfants voyants et non-voyants,
+                          en développant leur autonomie et leur mobilité et en
+                          encourageant leur confiance en eux et à s’ouvrir au
+                          monde.
+                        </p>
                         <div class="image_presentation"></div>
                     </div>
                     <div class="presentation_bas">
                         <div class="presentation_droite">
                             <h3 class="titre_secondaire texte_fonce">Historique</h3>
-                            <br />
-                            <p class="texte_fonce">Nathan est né en 2007 en créant la surprise : il ne verrait jamais car ses nerfs optiques étaient trop fins pour laisser passer une quelconque information à son cerveau.
-                                <br /> Mais bien vite ses parents se sont rendu compte qu’il pouvait voir avec son cœur !
-                                <br /> Convaincus, amis et proches de la famille se sont dit que quelques paires de bras supplémentaires ne seraient pas superflues pour lui permettre de développer son don et expliquer aux autres de quoi il retourne.</p>
-                            <br />
-                            <p class="texte_fonce">Et c’est ainsi que nous avons décidé de créer l’association Vue du cœur : à plusieurs, ça devient une aventure, là où seuls cela peut être la galère.
-                                <br /> Depuis avril 2012, Nathan nous guide et à travers lui nous découvrons ensemble le monde de la non-voyance.
-                                <br /> Nous essayons de sensibiliser à cette différence et de partager nos expériences.
-                                <br /> Bien sur, l’association cherche des fonds afin de financer les onéreux matériels et livres adaptés.
-                                <br /> Mais aucune somme d’argent ne remplacera l’ingéniosité, les trucs et astuces, et les conseils de ceux qui œuvrent de leur main pour pallier le manque de ce qui pour les enfants ordinaires est tout simplement normal.</p>
+
+                            <p class="texte_fonce">Nathan est né en 2007 en créant
+                              la surprise : il ne verrait jamais car ses nerfs
+                              optiques étaient trop fins pour laisser passer une
+                              quelconque information à son cerveau.</p>
+                            <p class="texte_fonce">Mais bien vite ses parents se
+                              sont rendu compte qu’il pouvait voir avec son cœur !
+                            </p>
+                            <p class="texte_fonce">Convaincus, amis et proches
+                              de la famille se sont dit que quelques paires de
+                              bras supplémentaires ne seraient pas superflues pour
+                              lui permettre de développer son don et expliquer
+                              aux autres de quoi il retourne.
+                            </p>
+
+                            <p class="texte_fonce">Et c’est ainsi que nous avons
+                              décidé de créer l’association Vue du cœur : à
+                              plusieurs, ça devient une aventure, là où seuls
+                              cela peut être la galère.</p>
+                            <p class="texte_fonce">Depuis avril 2012, Nathan nous
+                              guide et à travers lui nous découvrons ensemble le
+                              monde de la non-voyance.</p>
+                            <p class="texte_fonce">Nous essayons de sensibiliser
+                              à cette différence et de partager nos expériences.
+                            </p>
+                            <p class="texte_fonce">Bien sur, l’association cherche
+                              des fonds afin de financer les onéreux matériels et
+                              livres adaptés.
+                            </p>
+                            <p class="texte_fonce">Mais aucune somme d’argent ne
+                              remplacera l’ingéniosité, les trucs et astuces, et
+                              les conseils de ceux qui œuvrent de leur main pour
+                              pallier le manque de ce qui pour les enfants ordinaires
+                              est tout simplement normal.
+                            </p>
                         </div>
                         <div class="cadre_jaune">
                             <div class="texte_cadre_jaune">
                                 <h3 class="texte_jaune titre_secondaire">Nos membres</h3>
-                                <br />
-                                <p class="texte_clair"> <span>Nom Prenom</span> - Fonction
-                                    <br /> 06.00.00.00.00
-                                    <br /> adresse@mail.com </p>
-                                <br />
-                                <p class="texte_clair"> <span>Nom Prenom</span> - Fonction
-                                    <br /> 06.00.00.00.00
-                                    <br /> adresse@mail.com </p>
-                                <br />
-                                <p class="texte_clair"> <span>Nom Prenom</span> - Fonction
-                                    <br /> 06.00.00.00.00
-                                    <br /> adresse@mail.com </p>
-                                <br />
-                                <p class="texte_clair"> <span>Nom Prenom</span> - Fonction
-                                    <br /> 06.00.00.00.00
-                                    <br /> adresse@mail.com </p>
+
+                                <p class="texte_clair"> <span>Nom Prenom</span> - Fonction</p>
+                                <p class="texte_clair">06.00.00.00.00</p>
+                                <p class="texte_clair">adresse@mail.com</p>
+
+                                <p class="texte_clair"> <span>Nom Prenom</span> - Fonction</p>
+                                <p class="texte_clair">06.00.00.00.00</p>
+                                <p class="texte_clair">adresse@mail.com</p>
+
+                                <p class="texte_clair"> <span>Nom Prenom</span> - Fonction</p>
+                                <p class="texte_clair">06.00.00.00.00</p>
+                                <p class="texte_clair">adresse@mail.com </p>
+
+                                <p class="texte_clair"> <span>Nom Prenom</span> - Fonction</p>
+                                <p class="texte_clair">06.00.00.00.00</p>
+                                <p class="texte_clair">adresse@mail.com</p>
                             </div>
                         </div>
                     </div>
