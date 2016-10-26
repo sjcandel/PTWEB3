a8a02f7adcef60c4431ef9b57290eb4ee5ecb798
Talinona1 <timothe.golla@etu.unistra.fr>
2016-10-26 17:11:13 +0200

    début archives

--- pages/archives.php
+++ pages/archives.php
@@ -2,7 +2,7 @@
 <html lang="fr">
     <!--HEADER-->
     <?php include("../includes/header.php"); ?>
-    
+
     <body>
         <!--NAV-->
         <?php include("../includes/nav.php"); ?>
@@ -12,9 +12,30 @@
         <main>
            <div class="wrapper-bleu">
 
-                <!-- insérez votre contenu ici -->
+             <div id="barre_recherche">
+                <form action="">
+                  <input class="bouton" type="button" value="<i class="fa fa-search" aria-hidden="true"></i>" />
+                  <input class="champ" type="text" placeholder="Rechercher"/>
+                </form>
+              </div>
 
-           </div> 
+              <div class="block">
+                <div class="partie_visible">
+                  En ce dimanche 16octobre, dans la brume d’abord, puis par un beau soleil, Mathéo, Mathilde, Nael et  Orela ont fait la connaissance de la joëlette.
+                </div>
+                <div class="block-hidden">
+                  Nathan avait eu l’occasion de l’expérimenter à maintes reprises, toujours avec ravissement et ses camarades en sont tout aussi ravis.
+                  Nos amis de l’association "Osons la différence", secondés par les « Dunes d’espoir », habitués à transporter des enfants en situation de handicap moteur, ont offert ce tour dans la nature à un public d’enfants mal et non-voyants.  Ils ont osé la différence pour le plus grand bonheur de nos participants.
+                  Les enfants sont montés à tour de rôle, un peu étonnés pour certains, mais bien rapidement  conquis  par ces sensations nouvelles.
+                  Il y a eu d’autres surprises: le rallye dans la nature automnale et la montée dans l’enceinte du château fort, au son de l’épinette de Alain Christophe, troubadour pour ce jour.
+                  Un pique-nique dans un air un peu vif; au retour était programmé un essai avec les GPS mais  au diable le progrès, place à ce véhicule magique.
+                  Les associations partenaires sont reparties  pour une après-midi sportive et  donner le sourire à Valentin. Merci à eux… et vraiment, ça vaut la peine : « osons la différence »
+                </div>
+                <div class="block-visible">
+                  En savoir plus
+                </div>
+              </div>
+           </div>
         </main>
 
 
