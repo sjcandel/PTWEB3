334e5727b730b192ad5370cb4f5f2f64dac3cf34
Sophie <sophiee.candel@free.fr>
2016-11-01 20:09:03 +0100

    01/11 - 20h09
    • correction fichier "timothe.css"
    • mise en forme fichier "timothe.css"
    • transfert vers "style.css"
    • suppression du fichier "timothe.css"

--- includes/header.php
+++ includes/header.php
@@ -4,8 +4,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <link rel="stylesheet" href="../styles/reset.css">
     <link rel="stylesheet" href="../styles/font-awesome-4.6.3/css/font-awesome.css">
-    <link rel="stylesheet" href="../styles/animate.css"> 
     <link rel="stylesheet" href="../styles/style.css">
-    <link rel="stylesheet" href="../styles/timothe.css">
     <link rel="stylesheet" href="../styles/responsive.css">
 </head>
