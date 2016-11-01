0374fc569d04cbd1ba35b92e3ffa6e805aa28012
Sophie <sophiee.candel@free.fr>
2016-11-01 18:53:02 +0100

    1/11 - 18h52
    • page contact : correction HTML/CSS + responsive

--- pages/contact.php
+++ pages/contact.php
@@ -18,46 +18,40 @@
                 <div class="flex_item">
                     <form action="reception.php" method="post">
                     <!--<div class="flex_itm">-->
-                        <div class="">
-                            <input type="text" class="form-control" name="nom" maxlength="120" placeholder="Nom...">
-                        </div>
-
-                        <div class="">
-                            <input type="text" class="form-control" name="email" maxlength="256" placeholder="E-mail...">
-                        </div>
-                        <div class="">
-                            <input type="text" class="form-control" name="objet" maxlength="256" placeholder="Objet...">
-                        </div> 
-                        <div class="">
-                            <textarea type="text" class="form-control" name="message" placeholder="Message..."></textarea>
-                        </div>
+                        <input type="text" class="form-control" name="nom" placeholder="Nom...">
+                        <input type="text" class="form-control" name="email" placeholder="E-mail...">
+                        <input type="text" class="form-control" name="objet" placeholder="Objet...">
+                        <textarea type="text" class="form-control" name="message" placeholder="Message..."></textarea>
+                        
                     <!--</div>-->
-                    <button type="submit" class="butt_form">Envoyer</button>
+                    <div type="submit" class="bouton-jaune-upp">Envoyer</div>
                     </form>
                 </div>
 
-                <div class="flex_item flex_don">
-                    <h3 class="titre_secondaire titre_don">Faire un don</h3>
-                    <p class="texte_clair texte"> Votre solidarité nous est indispensable ! </p>
-                        <p class="texte_clair flex_texte"> 30 € </p>
-                        <p class="texte_clair flex_texte"> 50 € </p>
-                        <p class="texte_clair flex_texte"> 75 € </p>
-                        <p class="texte_clair flex_texte"> 100 € </p>
-                        <p class="texte_clair flex_texte"> autre montant </p>
-                    <button class="texte_clair butt_texte">donner</button>
+                <div class="flex_item flex_don texte_clair">
+                    <h3 class="titre_secondaire texte_jaune">Faire un don</h3>
+                    <p> Votre solidarité nous est indispensable ! </p>
+                       <div class="flex_texte">
+                            <p> 30 € </p>
+                            <p> 50 € </p>
+                            <p> 75 € </p>
+                            <p> 100 € </p>
+                            <p> autre montant </p>
+                            </div>
+                    <div class="bouton-jaune-upp">donner</div>
                 </div>
             </div>
 
             <h2 class="titre_principal"> Nos partenaires </h2>
 
-            <div class="part">
+            <div class="flex_contain">
                 <div class="flex_part">
                    <div class="div_part"> 
                         <img src="../images/part1.jpg">
                     </div>
                     <div class="div_part">
                         <div class="part_texte">
-                        <p class="nom_part">Osons La Différence</p>
+                        <p class="titre-secondaire">Osons La Différence</p>
                         <p class="lien"> <a href="http://www.osonsladifference.org/" target="_blank">http://www.osonsladifference.org/</a> </p>
                         </div>
                    </div> 
@@ -69,7 +63,7 @@
                     </div>
                     <div class="div_part">
                         <div class="part_texte">
-                        <p class="nom_part">Vue d'ensemble</p>
+                        <p class="titre-secondaire">Vue d'ensemble</p>
                         <p class="lien"> <a href="http://www.vue-densemble.com/" target="_blank">http://www.vue-densemble.com/</a> </p>
                         </div>
                    </div> 
@@ -81,7 +75,7 @@
                     </div>
                    <div class="div_part">
                         <div class="part_texte">
-                        <p class="nom_part">Yvoir</p>
+                        <p class="titre-secondaire">Yvoir</p>
                         <p class="lien"> <a href="http://parispekin.yvoir.free.fr/index.htm" target="_blank">http://parispekin.yvoir.free.fr/index.htm/</a> </p>
                         </div>
                    </div>
@@ -93,7 +87,7 @@
                     </div>
                     <div class="div_part">
                         <div class="part_texte">
-                        <p class="nom_part">Quatz Trotters</p>
+                        <p class="titre-secondaire">Quatz Trotters</p>
                         <p class="lien"> <a href="https://www.facebook.com/Quatz-Trotters-167437973276193/" target="_blank">https://www.facebook.com/Quatz-Trotters-167437973276193/</a> </p>
                         </div>
                    </div>
