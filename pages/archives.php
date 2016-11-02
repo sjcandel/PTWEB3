88e4b0a3aa9a0e7f129753b821ba108f203a1d3b
Talinona1 <timothe.golla@etu.unistra.fr>
2016-11-02 12:35:55 +0100

    enlevage des class="texte_fonce"

--- pages/archives.php
+++ pages/archives.php
@@ -44,12 +44,12 @@
                                     <p class="texte_fonce"> En ce dimanche 16 octobre, dans la brume d’abord, puis par un beau soleil, Mathéo, Mathilde, Nael et Orela ont fait la connaissance de la joëlette. </p>
                                 </div>
                                 <div class="block-hidden">
-                                    <p class="texte_fonce"> Nathan avait eu l’occasion de l’expérimenter à maintes reprises, toujours avec ravissement et ses camarades en sont tout aussi ravis. </p>
-                                    <p class="texte_fonce"> Nos amis de l’association "Osons la différence", secondés par les « Dunes d’espoir », habitués à transporter des enfants en situation de handicap moteur, ont offert ce tour dans la nature à un public d’enfants mal et non-voyants. Ils ont osé la différence pour le plus grand bonheur de nos participants. </p>
-                                    <p class="texte_fonce"> Les enfants sont montés à tour de rôle, un peu étonnés pour certains, mais bien rapidement conquis par ces sensations nouvelles. </p>
-                                    <p class="texte_fonce"> Il y a eu d’autres surprises: le rallye dans la nature automnale et la montée dans l’enceinte du château fort, au son de l’épinette de Alain Christophe, troubadour pour ce jour. </p>
-                                    <p class="texte_fonce"> Un pique-nique dans un air un peu vif; au retour était programmé un essai avec les GPS mais au diable le progrès, place à ce véhicule magique. </p>
-                                    <p class="texte_fonce"> Les associations partenaires sont reparties pour une après-midi sportive et donner le sourire à Valentin. Merci à eux… et vraiment, ça vaut la peine : « osons la différence » </p>
+                                    <p> Nathan avait eu l’occasion de l’expérimenter à maintes reprises, toujours avec ravissement et ses camarades en sont tout aussi ravis. </p>
+                                    <p> Nos amis de l’association "Osons la différence", secondés par les « Dunes d’espoir », habitués à transporter des enfants en situation de handicap moteur, ont offert ce tour dans la nature à un public d’enfants mal et non-voyants. Ils ont osé la différence pour le plus grand bonheur de nos participants. </p>
+                                    <p> Les enfants sont montés à tour de rôle, un peu étonnés pour certains, mais bien rapidement conquis par ces sensations nouvelles. </p>
+                                    <p> Il y a eu d’autres surprises: le rallye dans la nature automnale et la montée dans l’enceinte du château fort, au son de l’épinette de Alain Christophe, troubadour pour ce jour. </p>
+                                    <p> Un pique-nique dans un air un peu vif; au retour était programmé un essai avec les GPS mais au diable le progrès, place à ce véhicule magique. </p>
+                                    <p> Les associations partenaires sont reparties pour une après-midi sportive et donner le sourire à Valentin. Merci à eux… et vraiment, ça vaut la peine : « osons la différence » </p>
                                 </div>
                                 <div class="bouton-jaune"> Lire la suite </div>
                             </div>
@@ -58,24 +58,24 @@
                                 <div class="partie_visible">
                                     <h3 class="titre-secondaire">Kombissiri au cinéma le VOX</h3>
                                     <p class="archives_date"> BY CLARISSE · 4 OCTOBRE 2016 </p>
-                                    <p class="texte_fonce"> Réservez le 12 octobre pour le film documentaire de Kombissiri. </p>
-                                    <p class="texte_fonce"> C’est un beau regard sur l’Afrique : on y admire le formidable travail qu’y ont réalisé les Association Yvoir et Vélo pour le Faso. Les deux associations et toute l’équipe de tournage ont fait un travail remarquable, dans des conditions pas toujours faciles. </p>
+                                    <p> Réservez le 12 octobre pour le film documentaire de Kombissiri. </p>
+                                    <p> C’est un beau regard sur l’Afrique : on y admire le formidable travail qu’y ont réalisé les Association Yvoir et Vélo pour le Faso. Les deux associations et toute l’équipe de tournage ont fait un travail remarquable, dans des conditions pas toujours faciles. </p>
                                 </div>
                                 <div class="block-hidden">
-                                    <p class="texte_fonce"> Yvoir est une association partenaire qui soutient et accompagne « Vue du coeur » </p>
-                                    <p class="texte_fonce"> Vous trouverez ci-dessous l’invitation de Gérard et François. </p>
-                                    <p class="texte_fonce"> Chers amis, </p>
-                                    <p class="texte_fonce"> Yvoir et Vélos pour le Faso ont le plaisir de vous convier à la première projection du film « Kombissiri » à Strasbourg le 12 octobre prochain. </p>
-                                    <p class="texte_fonce"> Ce film extraordinaire, réalisé par René Letzgus (star production) avait été diffusé en mai dernier en ouverture du festival « Cannes Cinéphile ». Il relate notamment le travail de Yvoir et Vélos pour le Faso au Burkina Faso, où nous avons notamment créé la première course handisport en tandem d’Afrique de l’Ouest. </p>
-                                    <p class="texte_fonce"> Ce film revêt une importance particulière pour nos deux associations puisqu’il permet de lever des fonds afin d’opérer toujours plus de personnes de la cataracte à l’hôpital de Kombissiri. Ce sont déjà des dizaines de personnes qui vont pouvoir être opérées et retrouver la vue grâce a lui. </p>
-                                    <p class="texte_fonce"> Il sera projeté le 12 octobre prochain à partir de 19h45 au cinéma Vox à Strasbourg. </p>
-                                    <p class="texte_fonce"> Nous espérons vous y retrouver le plus nombreux possible ! Et nous comptons sur vous pour faire connaître cet événement autour de vous. </p>
-                                    <p class="texte_fonce"> Avec toute notre amitié </p>
-                                    <p class="texte_fonce"> Gérard Muller, président d’Yvoir et François Orsat, prèsident de Vélos pour le Faso </p>
-                                    <p class="texte_fonce"> Avec toute notre amitié </p>
-                                    <p class="texte_fonce"> Suivez-nous sur Facebook : </p>
-                                    <p class="texte_fonce"> Association Yvoir – Gérard Muller </p>
-                                    <p class="texte_fonce"> Pour soutenir nos actions au Burkina Faso : </p> <a href="https://www.helloasso.com/associations/association-yvoir/collectes/kombissiri">https://www.helloasso.com/associations/association-yvoir/collectes/kombissiri</a> </div>
+                                    <p> Yvoir est une association partenaire qui soutient et accompagne « Vue du coeur » </p>
+                                    <p> Vous trouverez ci-dessous l’invitation de Gérard et François. </p>
+                                    <p> Chers amis, </p>
+                                    <p> Yvoir et Vélos pour le Faso ont le plaisir de vous convier à la première projection du film « Kombissiri » à Strasbourg le 12 octobre prochain. </p>
+                                    <p> Ce film extraordinaire, réalisé par René Letzgus (star production) avait été diffusé en mai dernier en ouverture du festival « Cannes Cinéphile ». Il relate notamment le travail de Yvoir et Vélos pour le Faso au Burkina Faso, où nous avons notamment créé la première course handisport en tandem d’Afrique de l’Ouest. </p>
+                                    <p> Ce film revêt une importance particulière pour nos deux associations puisqu’il permet de lever des fonds afin d’opérer toujours plus de personnes de la cataracte à l’hôpital de Kombissiri. Ce sont déjà des dizaines de personnes qui vont pouvoir être opérées et retrouver la vue grâce a lui. </p>
+                                    <p> Il sera projeté le 12 octobre prochain à partir de 19h45 au cinéma Vox à Strasbourg. </p>
+                                    <p> Nous espérons vous y retrouver le plus nombreux possible ! Et nous comptons sur vous pour faire connaître cet événement autour de vous. </p>
+                                    <p> Avec toute notre amitié </p>
+                                    <p> Gérard Muller, président d’Yvoir et François Orsat, prèsident de Vélos pour le Faso </p>
+                                    <p> Avec toute notre amitié </p>
+                                    <p> Suivez-nous sur Facebook : </p>
+                                    <p> Association Yvoir – Gérard Muller </p>
+                                    <p> Pour soutenir nos actions au Burkina Faso : </p> <a href="https://www.helloasso.com/associations/association-yvoir/collectes/kombissiri">https://www.helloasso.com/associations/association-yvoir/collectes/kombissiri</a> </div>
                                 <div class="bouton-jaune"> Lire la suite </div>
                             </div>
                             
@@ -85,12 +85,12 @@
                                 <div class="partie_visible">
                                     <h3 class="titre-secondaire">Première séance de jeux à l’orangerie</h3>
                                     <p class="archives_date"> BY CLARISSE · 1 OCTOBRE 2016 </p>
-                                    <p class="texte_fonce"> Ils étaient cinq à répondre présents à l’appel de Clément et Nicolas. Dion, Mathéo, Nael, Nathan et Orela. Prêts à vouloir apprendre à se servir du GPS comme leur champion, comme leurs modèles. </p>
+                                    <p> Ils étaient cinq à répondre présents à l’appel de Clément et Nicolas. Dion, Mathéo, Nael, Nathan et Orela. Prêts à vouloir apprendre à se servir du GPS comme leur champion, comme leurs modèles. </p>
                                 </div>
                                 <div class="block-hidden">
-                                    <p class="texte_fonce"> Mais Clément a expliqué que avant de se servir de l’outil de navigation, il faut se servir de ses jambes, de sa tête, autrement dit, savoir mesurer des distances en comptant ses pas, savoir suivre et retrouver quelqu’un ..savoir tourner comme une montre. Tout cela ce soir-là en s’amusant, en marquant des points. Tout cela aussi en jouant ensemble, voyants et non voyants pour apprendre les uns des autres. </p>
-                                    <p class="texte_fonce"> Une soirée toute douce, on aurait dit que l’été nous a attendus avant de tirer sa révérence, nous càd notre petit groupe, les parents frères et sœurs et des bénévoles de « Vue du cœur ». </p>
-                                    <p class="texte_fonce"> On a ainsi pu jouer les prolongations et ce n’est qu’à la tombée de la nuit que le groupe s’est séparé. Promis » on reviendra »! Nous aussi pour la prochaine séance au début des vacances de la Toussaint Et si on arrivait à vous donner l’envie de vous retrouver et vous donner un peu des ailes … </p>
+                                    <p> Mais Clément a expliqué que avant de se servir de l’outil de navigation, il faut se servir de ses jambes, de sa tête, autrement dit, savoir mesurer des distances en comptant ses pas, savoir suivre et retrouver quelqu’un ..savoir tourner comme une montre. Tout cela ce soir-là en s’amusant, en marquant des points. Tout cela aussi en jouant ensemble, voyants et non voyants pour apprendre les uns des autres. </p>
+                                    <p> Une soirée toute douce, on aurait dit que l’été nous a attendus avant de tirer sa révérence, nous càd notre petit groupe, les parents frères et sœurs et des bénévoles de « Vue du cœur ». </p>
+                                    <p> On a ainsi pu jouer les prolongations et ce n’est qu’à la tombée de la nuit que le groupe s’est séparé. Promis » on reviendra »! Nous aussi pour la prochaine séance au début des vacances de la Toussaint Et si on arrivait à vous donner l’envie de vous retrouver et vous donner un peu des ailes … </p>
                                 </div>
                                 <div class="bouton-jaune"> Lire la suite </div>
                             </div>
@@ -101,13 +101,13 @@
                                 <div class="partie_visible">
                                     <h3 class="titre-secondaire">« Vue du cœur » fait sa rentrée: le calendrier</h3>
                                     <p class="archives_date"> BY CLARISSE · 20 SEPTEMBRE 2016 </p>
-                                    <p class="texte_fonce"> En ce mois de la rentrée, l’actualité de Vue du cœur et de ses partenaires est bien chargée. </p>
+                                    <p> En ce mois de la rentrée, l’actualité de Vue du cœur et de ses partenaires est bien chargée. </p>
                                 </div>
                                 <div class="block-hidden">
-                                    <p class="texte_fonce"> Tout d’abord l’association s’est vu remettre un chèque de 1000 euros par les Quatz trotters, ce qui correspond à un euro par dossard participant au trail du Kochersberg. Nous vous en avions informés ici-même. Les enfants déficients visuels avaient participé sous la pluie et avec bonne humeur. Clément y avait réalisé l’exploit de terminer une boucle de 26 km en autonomie, pataugeant dans la boue et slalomant entre les hautes herbes avec sa canne et son GPS. </p>
-                                    <p class="texte_fonce"> Un grand merci à l’équipe de Jean-Guy animée, non seulement d’un bel esprit sportif, mais d’une dynamique solidaire et acquise à la cause visuelle. Le TDK se nomme le « typhlo trail », c’est tout dire. </p>
-                                    <p class="texte_fonce"> Cet argent servira à l’acquisition de matériel spécifique et numérique pour les projets en cours. </p>
-                                    <p class="texte_fonce"> En effet à partir du 28 septembre, nous nous retrouvons avec Clément et Nicolas de « Vue d’ensemble » pour une animation et des jeux à l’orangerie avec ces enfants qui ont marché et qui veulent très bientôt égaler les plus grands. </p>
+                                    <p> Tout d’abord l’association s’est vu remettre un chèque de 1000 euros par les Quatz trotters, ce qui correspond à un euro par dossard participant au trail du Kochersberg. Nous vous en avions informés ici-même. Les enfants déficients visuels avaient participé sous la pluie et avec bonne humeur. Clément y avait réalisé l’exploit de terminer une boucle de 26 km en autonomie, pataugeant dans la boue et slalomant entre les hautes herbes avec sa canne et son GPS. </p>
+                                    <p> Un grand merci à l’équipe de Jean-Guy animée, non seulement d’un bel esprit sportif, mais d’une dynamique solidaire et acquise à la cause visuelle. Le TDK se nomme le « typhlo trail », c’est tout dire. </p>
+                                    <p> Cet argent servira à l’acquisition de matériel spécifique et numérique pour les projets en cours. </p>
+                                    <p> En effet à partir du 28 septembre, nous nous retrouvons avec Clément et Nicolas de « Vue d’ensemble » pour une animation et des jeux à l’orangerie avec ces enfants qui ont marché et qui veulent très bientôt égaler les plus grands. </p>
                                     <p class="texte_fonce"> D’ailleurs nous sommes à la recherche de bénévoles encadrants- une fois par mois, si le cœur vous en dit. </p>
                                     <p class="texte_fonce"> Et le 16 octobre nous ferons une sortie vers le Haut Barr avec l’Association « Osons la différence » tour de joëlette et bonne humeur garantis. </p>
                                     <p class="texte_fonce"> Si vous souhaitez participer, n’hésitez pas à nous rejoindre. </p>
